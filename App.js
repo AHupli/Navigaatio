a1b78b777073ffcdc3fbbb9909901176a4a5b59a
Antti Hupli <t1huan00@students.oamk.fi>
2023-09-25 16:05:47 +0300

    Pitäisi toimia

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './HomeScreen';
+import React from 'react';
+import SecondScreen from './SecondScreen';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          title: 'Home',
+          headerTitle: 'Home',
+        }}
+        />
+        <Stack.Screen
+        name="Second"
+        component={SecondScreen}
+        option={{
+          title: 'Second',
+          headerTitle: 'Second'
+        }}
+        />
+      </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
